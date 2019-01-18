b872406a488772a966b38edc2cb0ba66126d34ff
zul <fahrizalmursalin06@gmai.com>
2019-01-18 21:27:35 +0700

    initial commit

--- app/src/main/java/com/example/msi/cv/ContactMe2.java
+++ app/src/main/java/com/example/msi/cv/ContactMe2.java
@@ -1,16 +1,12 @@
 package com.example.msi.cv;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import static java.lang.Double.parseDouble;
 
 public class ContactMe2 extends AppCompatActivity {
 
--- app/src/main/java/com/example/msi/cv/MainActivity.java
+++ app/src/main/java/com/example/msi/cv/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.msi.cv;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
